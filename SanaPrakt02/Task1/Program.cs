6d46fa5e84a6ae2ec7cebae61ac9cc1e54f9a7d8
Alina Ilchenko <ilchenkoalina888@gmail.com>
2022-11-10 10:36:29 +0200

    cs changed

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Learning Git");
+Console.WriteLine("*************");
+Console.WriteLine("Alina Ilchnko");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
